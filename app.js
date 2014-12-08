cefcc331b2d19b0d761bef5e4c1cd193a1e10cbd
Samanta <samanta.aires@ymail.com>
2014-12-08 20:24:48 -0300

    Codificacao na troca de informacao

---
+++ app.js
@@ -0,0 +1,53 @@
+
+var name = prompt("Valor Binário");
+
+int tam = name.length();
+
+var unipolar = function(value){
+	
+	if(value !== 0 && value !== 1) {
+		console.log("Entrada inválida");
+
+	} else if(value == 0) {
+		return ;
+	
+	} else {
+		return ;
+	}
+};
+
+var bipolarNRZ = function(value){
+	if(value !== 0 && value !== 1) {
+		console.log("Entrada inválida");
+
+	} else if(value == 0) {
+		return ;
+	
+	} else {
+		return ;
+	}
+};
+
+var bipolarRZ = function(value){
+	if(value !== 0 && value !== 1) {
+		console.log("Entrada inválida");
+
+	} else if(value == 0) {
+		return ;
+	
+	} else {
+		return ;
+	}
+};
+
+var manchester = function(value){
+	if(value !== 0 && value !== 1) {
+		console.log("Entrada inválida");
+
+	} else if(value == 0) {
+		return ;
+	
+	} else {
+		return ;
+	}
+};
